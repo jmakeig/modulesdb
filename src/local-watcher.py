c69d80a4d00dc8f638cddfa72214be4b584905a2
Justin Makeig <jmpublic@makeig.com>
2013-04-30 22:16:31 -0700

    Clean up.

--- src/local-watcher.py
+++ src/local-watcher.py
@@ -174,7 +174,6 @@ def observe(directory, recursive=True, exclusions=[]):
             time.sleep(1) 
     except KeyboardInterrupt: 
         observer.stop()
-        # sys.exit(0)
     # All of the examples call .join(). I don't see how this is ever exectued, though, except right after the interrupt is handled. What does join do?
     observer.join()
 
@@ -201,13 +200,7 @@ if __name__ == '__main__':
         parser.add_argument('--password', '-p', default=None, help="The password to use to authenticate against the REST service. If the authentication method requires a password and you don't supply one at the command line or in the prefrences file you will be prompted for it.")
         #parser.add_argument('--cert', "-E", help="(SSL) Tells curl to use the specified client certificate file when getting a file with HTTPS, FTPS or another SSL-based protocol. The certificate must be in PEM format. If the optional password isn't specified, it will be queried for on the terminal. Note that this option assumes a \"certificate\" file that is the private key and the private certificate concatenated! See --cert and --key to specify them independently.")
         #parser.add_argument('--key', help="(SSL/SSH) Private key file name. Allows you to provide your private key in this separate file.")
-        #parser.add_argument('--insecure', "-k")
-
-        # class PermAction(argparse.Action):
-        #     def __call__(self, parser, namespace, values, option_string=None):
-        #         print '%r %r %r' % (namespace, values, option_string)
-        #         setattr(namespace, self.dest, values)
-                
+        #parser.add_argument('--insecure', "-k")                
         parser.add_argument('--permission', '-perm', dest="permissions", nargs="?", action="append", default=None, help="The permissions that should be set on all files.")
         parser.add_argument('--ignore', nargs="?", action="append", default=None, help="A gitignore-style path to ignore for observation and walking.")
         
